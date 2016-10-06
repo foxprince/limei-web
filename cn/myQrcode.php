0d0768e9a8534455a6a1a8ea870e6a8c539fa768
zj-dell <9600348@qq.com>
2016-10-06 10:18:50 +0800

    add qrcode on web

---
+++ cn/myQrcode.php
@@ -0,0 +1,148 @@
+<?php
+session_start ();
+
+if (! isset ( $_SESSION ['useraccount'] )) {
+	header ( 'Locaton: login.php' );
+	exit ();
+}
+
+$userid = $_SESSION ['useraccount'];
+
+require_once ('../includes/connection.php');
+$conn = dbConnect ( 'write', 'pdo' );
+$conn->query ( "SET NAMES 'utf8'" );
+
+$user_info = 'SELECT * FROM clients_list WHERE id = "' . $userid . '"';
+foreach ( $conn->query ( $user_info ) as $r_u ) {
+	$website_username = $r_u ['website_username'];
+	$website_password = $r_u ['website_password'];
+}
+include_once ('includes/header_ele.php');
+?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1" />
+<meta http-equiv="X-UA-Compatible" content="IE=9" />
+<title>利美钻石 - 我的二维码</title>
+<style type="text/css">
+div.history-choice {
+	border-bottom-style: solid;
+	border-width: 1px;
+	border-color: #999;
+	padding: 20px 0;
+}
+
+img.demo-pic {
+	display: block;
+	float: left;
+	width: 228px;
+	margin: 25px 25px 0 0;
+}
+
+div.dia-para-box {
+	float: left;
+	width: 358px;
+	font-size: 14px;
+}
+
+.dia-para-box h3 {
+	font-size: 18px;
+}
+
+.dia-para-box p {
+	font-size: 14px;
+	margin: 0 0 3px 0;
+}
+
+div#account-setting-form-box {
+	position: relative;
+	width: 288px;
+	padding: 15px;
+	background-color: #FFF;
+	border-style: solid;
+	border-width: 1px;
+	border-color: #999;
+	margin-left: 158px;
+	margin-top: 25px;
+	margin-bottom: 50px;
+}
+
+p.feedbackmessage, p.errormessage {
+	font-size: 24px;
+	color: #C30;
+}
+</style>
+</head>
+<body>
+  <!--div  home-visual-box-->
+  <div class="container-fluidX container maxcontainer">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12">
+<?php
+include_once ('includes/header.php');
+?>
+</div>
+    </div>
+    <!--div  bodycontent----------------------------------------------------------------------------------------->
+    <div id="bodycontent">
+      <div id="loginbox">
+
+<?php
+if (isset ( $errormessage )) {
+	?>
+<p class="errormessage"><?php echo $errormessage; ?></p>
+<?php
+}
+?>
+
+<?php
+if (isset ( $feedbackmessage )) {
+	?>
+<p class="feedbackmessage"><?php echo $feedbackmessage; ?></p>
+<?php
+}
+?>
+
+<h3>我的二维码</h3>
+        <a
+          style="font-size: 14px; display: inline-block; margin-left: 25px; padding: 5px 12px; background-color: #eee; position: relative; top: -3px;"
+          href="manageaccount.php">修改用户名密码</a> <a
+          style="font-size: 14px; display: inline-block; margin-left: 25px; padding: 5px 12px; background-color: #eee; position: relative; top: -3px;"
+          href="myQrcode.php">我的二维码</a>
+
+
+<?php
+if ($r_u ['qrcode']) {
+	?>
+			<a id="qr_image" href="../admin/qrcode/<?php echo $row['qrcode'] ?>" target="_blank">查看</a><br />
+        <div class="history-choice">
+          <img src="../admin/qrcode/<?php echo $r_u['qrcode']; ?>" class="demo-pic" />
+          <div class="dia-para-box">
+            <?php
+	$sql_history = 'SELECT wechat_name FROM clients_list WHERE referee = "' . $r_u ['id'] . '"';
+	foreach ( $conn->query ( $sql_history ) as $row_history ) {
+		echo $row_history ['wechat_name'] . "<br/>";
+	}
+	?>
+          </div>
+          <!-- end dia-para-box -->
+          <br style="clear: both;" />
+        </div>
+<?php
+} else {
+	echo '您尚未被分配二维码';
+}
+?>
+
+
+
+
+</div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12">
+<?php
+include_once ('includes/footer.php');
+?>
+</div>
+    </div>
+  </div>
+</body>
