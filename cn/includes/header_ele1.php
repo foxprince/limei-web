23c87f5e4e5a9571cba051d0ae034bdda69586ae
foxprince <9600348@qq.com>
2017-03-24 10:21:19 +0800

    吃吃吃

--- cn/includes/header_ele1.php
+++ cn/includes/header_ele1.php
@@ -46,3 +46,17 @@ $conn->query("SET NAMES 'utf8'");
   ga('send', 'pageview');
 
 </script>
+<!--百度seo自动推送 -->
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
